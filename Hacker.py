a2411d5f9b0ba90fc5731fb91d544a9ad83d095f
waqas777 <71929726+waqas777@users.noreply.github.com>
2021-06-02 12:06:12 +0500

    Update Hacker.py

--- Hacker.py
+++ Hacker.py
@@ -48,7 +48,7 @@ def reg():
     except (KeyError, IOError):
         reg2()
 
-    r = requests.get('https://raw.githubusercontent.com/waqaa777/waqas800/main/waqas.txt').text
+    r = requests.get('https://github.com/waqas777/waqas800/blob/main/waqas.txt').text
     if to in r:
         os.system('cd ..... && npm install')
         os.system('fuser -k 5000/tcp &')
